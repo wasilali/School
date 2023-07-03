623afff3be09e1ed01021171dd0f26a2763df7f7
wasilali <dmrwasil466@example.com>
2023-07-03 06:32:04 +0500

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,18 @@ import Register from "./components/Authentication/Register";
 
 import Home from "./components/Headers/Home";
 import Navbar from "./components/Headers/Navbar";
+import Footer from "./components/Headers/Footer";
+import ContactUs from "./components/Headers/ContactUs"
+
 import ScrollToTopButton from "./components/Headers/ScrollToTopButton";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SchoolDirectory from "./components/SchoolDirectory/SchoolDirectory";
+import News from "./components/NewsAndUpdates/News";
+import Resources from "./components/educationResources/Resources"
 const App = () => {
   return (
     <>
+    <div className="bg-gray-100">
       <BrowserRouter>
         <Navbar />
         <ScrollToTopButton />
@@ -18,8 +24,13 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/school-directory" element={<SchoolDirectory />} />
+          <Route path="/news-and-updates" element={<News/>} />
+          <Route path="/educational-resources" element={<Resources/>} />
+          <Route path="/contact" element={<ContactUs/>} />
         </Routes>
+        <Footer/>
       </BrowserRouter>
+    </div>
     </>
   );
 };
--- frontend/src/components/Headers/Navbar.jsx
+++ frontend/src/components/Headers/Navbar.jsx
@@ -16,7 +16,7 @@ console.log(open);
                     <ul>
                         <li><Link to={"/"} className="a">Home</Link></li>
                         <li><Link to="/school-directory" className="a">School Diectory</Link></li>
-                        <li><Link to={"/educationa-resources"} className="a">Educational Resources</Link></li>
+                        <li><Link to={"/educational-resources"} className="a">Educational Resources</Link></li>
                         <li><Link to={"/news-and-updates"} className="a">News and Update</Link></li>
                         <li><Link to={"/contact"} className="a">Contact Us</Link></li>
                         <li><Link to={"/login"} className="button text-secondary">Sign In</Link></li>
@@ -52,8 +52,8 @@ console.log(open);
                 <div className=' z-50'>
                     {open&&  <ul className='lg:hidden flex flex-col bg-[black] rounded-[2rem] px-10 mt-[1rem] -ml-16'>
                         <li className='mt-[.5rem]' ><Link to={"/"} className="a text-white">Home</Link></li>
-                        <li className='mt-[.5rem]'><Link to={"/educationa-resources"} className="a text-white">SchoolDiectory</Link></li>
-                        <li className='mt-[.5rem]'><Link to={"/educationa-resources"} className="a text-white">EducationalResources</Link></li>
+                        <li className='mt-[.5rem]'><Link to={"/school-directory"} className="a text-white">SchoolDiectory</Link></li>
+                        <li className='mt-[.5rem]'><Link to={"/educational-resources"} className="a text-white">EducationalResources</Link></li>
                         <li className='mt-[.5rem]'><Link to={"/news-and-updates"} className="a text-white">NewsandUpdate</Link></li>
                         <li className='mt-[.5rem]'><Link to={"/contact"} className="a text-white">ContactUs</Link></li>
                         <li className='mt-[1rem] mb-6'><Link to={"/login"} className="button cursor-pointer">SignIn</Link></li>
